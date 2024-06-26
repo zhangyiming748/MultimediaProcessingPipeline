938b64ca6fb9777c0fd44be50edc8cc5b700e7b5
zen <zhangyiming748@gmail.com>
2024-06-26 19:09:36 +0800

    Windows下运行会产生奇怪的制表符

---
+++ ytdlp/unit_test.go
@@ -0,0 +1,29 @@
+package ytdlp
+
+import (
+	"Multimedia_Processing_Pipeline/constant"
+	"Multimedia_Processing_Pipeline/log"
+	"Multimedia_Processing_Pipeline/util"
+	"testing"
+)
+
+func init() {
+
+}
+
+// go test -v -run TestYTdlp
+func TestYTdlp(t *testing.T) {
+	p := &constant.Param{
+		Root:     "/mnt/c/Users/zen/Github",
+		Language: "English",
+		Pattern:  "mp4",
+		Model:    "base",
+		Location: "/mnt/c/Users/zen/Github",
+		Proxy:    "192.168.1.5:8889",
+	}
+	log.SetLog(p)
+	uris := util.ReadByLine("/mnt/c/Users/zen/Github/Multimedia_Processing_Pipeline/ytdlp/test.list")
+	for _, uri := range uris {
+		DownloadVideo(uri, *p)
+	}
+}
