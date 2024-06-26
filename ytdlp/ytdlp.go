949c69c34cb934018a276da914811a5d6996e617
zen <zhangyiming748@gmail.com>
2024-06-26 21:21:14 +0800

    重命名失败报错不退出

--- ytdlp/ytdlp.go
+++ ytdlp/ytdlp.go
@@ -2,13 +2,14 @@ package ytdlp
 
 import (
 	"Multimedia_Processing_Pipeline/constant"
+	"Multimedia_Processing_Pipeline/replace"
 	"Multimedia_Processing_Pipeline/util"
 	"fmt"
 	"log"
 	"os/exec"
 )
 
-func DownloadVideo(uri string, p constant.Param) (fp string) {
+func DownloadVideo(uri string, p constant.Param) (fp string, err error) {
 	defer func() {
 		if err := recover(); err != nil {
 			fmt.Println(err)
@@ -18,11 +19,12 @@ func DownloadVideo(uri string, p constant.Param) (fp string) {
 	msg := fmt.Sprintf("正在运行命令:%s", cmd.String())
 	destination, err := util.ExecCommand4YtdlpDestination(cmd, msg)
 	if err != nil {
-		return ""
+		log.Fatalf("命令运行产生错误:%v\n", err)
 	} else if destination == "" {
 		log.Fatalf("视频下载后找不到标题信息,命令原文:%s\n", cmd.String())
 	} else {
 		log.Printf("当前下载成功的文件标题:%s", destination)
 	}
-	return destination
+	destination, err = replace.Rename(destination)
+	return destination, nil
 }
