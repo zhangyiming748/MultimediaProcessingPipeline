5056bddf02523b4d76bc7050b0b9e1f1524814ea
zen <zhangyiming748@gmail.com>
2024-06-26 17:20:36 +0800

    记录日志

--- main.go
+++ main.go
@@ -11,5 +11,13 @@ func initConfig(p *constant.Param) {
 	sql.SetDatabase(p)
 }
 func main() {
-
+	p := &constant.Param{
+		Root:     "/mnt/c/Users/zen/Github/FastYt-dlp/joi2",
+		Language: "English",
+		Pattern:  "mp4",
+		Model:    "base",
+		Location: "/mnt/c/Users/zen/Github/FastYt-dlp/joi2",
+		Proxy:    "127.0.0.1:8889",
+	}
+	initConfig(p)
 }
