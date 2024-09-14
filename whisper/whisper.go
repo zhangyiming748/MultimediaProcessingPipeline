7ec36c8d3bbcf8c8653ca7108673121897518f8f
zen <hasee>
2024-09-14 08:54:05 +0800

    使用cuda加速

--- whisper/whisper.go
+++ whisper/whisper.go
@@ -8,13 +8,17 @@ import (
 	"os/exec"
 	"path/filepath"
 	"strings"
+	"time"
 )
 
 /*
 生成字幕后返回字幕的绝对路径
 */
 func GetSubtitle(fp string, p *constant.Param) string {
-	cmd := exec.Command("whisper", fp, "--model", p.GetModel(), "--model_dir", p.GetLocation(), "--output_format", "srt", "--prepend_punctuations", ",.?", "--language", p.GetLanguage(), "--output_dir", p.GetRoot(), "--verbose", "True")
+	cmd := exec.Command("whisper", "--device", "cuda", fp, "--model", p.GetModel(), "--model_dir", p.GetLocation(), "--output_format", "srt", "--prepend_punctuations", ",.?", "--language", p.GetLanguage(), "--output_dir", p.GetRoot(), "--verbose", "True")
+	//cmd := exec.Command("whisper",  fp, "--model", p.GetModel(), "--model_dir", p.GetLocation(), "--output_format", "srt", "--prepend_punctuations", ",.?", "--language", p.GetLanguage(), "--output_dir", p.GetRoot(), "--verbose", "True")
+	startTime := time.Now()
+	log.Printf("文件%v开始时间%v", fp, startTime.Format("20060102 15:04:05"))
 	msg := fmt.Sprintf("正在处理的文件:%s", fp)
 	err := util.ExecCommand(cmd, msg)
 	if err != nil {
@@ -22,5 +26,10 @@ func GetSubtitle(fp string, p *constant.Param) string {
 	}
 	fp = strings.Replace(fp, filepath.Ext(fp), ".srt", 1)
 	//replace.RemoveTrailingNewlines(fp)
+	endTime := time.Now()
+	log.Printf("文件%v开始时间%v", fp, endTime.Format("20060102 15:04:05"))
+	duration := endTime.Sub(startTime)
+	totalMinutes := duration.Seconds() / 60
+	log.Printf("文件%v总共用时: %.2f 分钟\n", fp, totalMinutes)
 	return fp
 }
