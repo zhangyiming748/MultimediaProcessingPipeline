a7947bbb958ef564e450c4b55cc2cbf805a4af26
zen <hasee>
2024-09-23 10:20:13 +0800

    测试文件里解决未成功转换的文件

--- merge/unit_test.go
+++ merge/unit_test.go
@@ -1,7 +1,9 @@
 package merge
 
 import (
-	"Multimedia_Processing_Pipeline/constant"
+	"os"
+	"path/filepath"
+	"strings"
 	"testing"
 )
 
@@ -11,6 +13,33 @@ func init() {
 
 // go test -v -run TestMerge
 func TestMerge(t *testing.T) {
-	p := new(constant.Param)
-	MkvWithAss("/media/zen/swap/telegram.mp4", p)
+	root := "E:\\Downloads\\joi\\done"
+	mp4s, _ := getMP4Files(root)
+	for _, mp4 := range mp4s {
+		Mp4WithSrt(mp4)
+	}
+}
+
+// getMP4Files 遍历指定目录，返回所有 mp4 文件的路径
+func getMP4Files(dir string) ([]string, error) {
+	var mp4Files []string
+
+	// 使用 Walk 函数遍历目录
+	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		// 检查文件是否是 mp4 文件
+		if !info.IsDir() && strings.HasSuffix(strings.ToLower(info.Name()), ".mp4") {
+			mp4Files = append(mp4Files, path)
+		}
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return mp4Files, nil
 }
