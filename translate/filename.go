b7e39b04f968a6f78ee9c13b7af046630414ed20
zen <hasee>
2025-03-04 09:53:08 +0800

    翻译核心函数单独放

--- translate/filename.go
+++ translate/filename.go
@@ -1,37 +1,8 @@
 package translateShell
 
 import (
-	"Multimedia_Processing_Pipeline/replace"
-	"log"
-	"os"
-	"path/filepath"
 	"regexp"
-	"strings"
 )
-
-/*
-接受绝对路径
-*/
-func TransName(filename string) string {
-	base := filepath.Base(filename) // 从绝对路径中提取文件名部分
-	dir := filepath.Dir(filename)
-	ext := filepath.Ext(filename)             // 从文件名提取扩展名部分
-	name := strings.Replace(base, ext, "", 1) //纯文件名
-	//log.Printf("base name is %v\ndir is %v\next is %v\nname is %v\n", base, dir, ext, name)
-	name = replaceEnglishSquareBrackets(name)
-	name = replace.ChinesePunctuation(name)
-	zh_cn, err := TransOnce(name, "192.168.1.20:8889")
-	if err != nil {
-		log.Println("translate err:", err)
-		return filename
-	}
-	zh_cn = strings.Replace(zh_cn, "\n", "", -1)
-	//fmt.Println("zh_ch is ", zh_cn)
-	n_name := strings.Join([]string{zh_cn, ext}, "")
-	n_path := strings.Join([]string{dir, n_name}, string(os.PathSeparator))
-	//strings.Join([]string{zh_cn, ext}, "")
-	return n_path
-}
 
 /*
 替换英文方括号
--- translate/translateShell.go
+++ translate/translateShell.go
@@ -15,8 +15,6 @@ import (
 	"strconv"
 	"strings"
 	"time"
-
-	"github.com/OwO-Network/DeepLX/translate"
 )
 
 const PREFIX = "https://api.deeplx.org"
@@ -30,35 +28,6 @@ const (
 	TIMEOUT = 10 //second
 
 )
-
-func TransByLinuxdoDeepLX(src, apikey string) (dst string) {
-	//apikey := os.Getenv("LINUXDO")
-	result, err := Req(src, apikey)
-	result = strings.Replace(result, "\\r\\n", "", 1)
-	result = strings.Replace(result, "\n", "", 1)
-	result = strings.Replace(result, "\r\n", "", 1)
-	if result == "" {
-		return
-	}
-	if err != nil {
-		log.Fatalln(err)
-	}
-	log.Printf("linuxdo 版本 deeplx 返回:%+v\n", result)
-	return result
-}
-
-func TransByGithubDeepLX(src string) (dst string) {
-	ret, err := translate.TranslateByDeepLX("auto", "zh", src, "", "", "")
-	if err != nil {
-		log.Fatalf("Github版本deeplx翻译报错%v\n", err)
-	}
-	log.Printf("GitHub 版本 deeplx 返回:%+v\n", ret)
-	dst = ret.Data
-	dst = strings.Replace(dst, "\\r\\n", "", 1)
-	dst = strings.Replace(dst, "\n", "", 1)
-	dst = strings.Replace(dst, "\r\n", "", 1)
-	return dst
-}
 
 func Translate(src string, p *constant.Param, c *constant.Count) (dst string) {
 	if p.LinuxDo == "" {
